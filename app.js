8401ba84036a605bfffeab1be833c09522653cdd
tapringSnapIt <tapring.knight@snapit.solutions>
2023-12-04 11:53:47 -0600

    Updated sample text for my app

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('My lab change!'));
+app.get('/', (req, res) => res.send('Updated text for my lab!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
